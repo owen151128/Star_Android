ee44cd99d79dcf6a75ffddad34049f24f6b58d41
koikjin <rhdlrwls11@nave.rcom>
2017-01-22 18:48:42 +0900

    tablaout 구현및 기반이되는 작업들

--- app/src/main/java/kr/pe/dreamer/startalk/MainActivity.java
+++ app/src/main/java/kr/pe/dreamer/startalk/MainActivity.java
@@ -1,7 +1,14 @@
 package kr.pe.dreamer.startalk;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        setSupportActionBar(toolbar);
+
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.tab_friend_icon_n));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.tab_chatting_icon_n));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(tabPagerAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.d("test" , "1");
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+
+    private class TabPagerAdapter extends FragmentPagerAdapter {
+        private int count;
+
+        public TabPagerAdapter(FragmentManager fm, int count) {
+            super(fm);
+            this.count = count;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    FriendFragment friendFragment = FriendFragment.newInstance();
+                    return friendFragment;
+                case 1:
+                    ChattingFragment chattingFragment = ChattingFragment.newInstance();
+                    return chattingFragment;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return count;
+        }
     }
 }

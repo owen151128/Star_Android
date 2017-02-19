c84a4cf71427a672316c184f27623f3aa80ee9dc
koikjin <rhdlrwls11@nave.rcom>
2017-02-19 19:36:51 +0900

    Image Crop 기능

--- app/src/main/java/kr/pe/dreamer/startalk/util/ImageLoad.java
+++ app/src/main/java/kr/pe/dreamer/startalk/util/ImageLoad.java
@@ -1,9 +1,11 @@
 package kr.pe.dreamer.startalk.util;
 
 import android.content.Context;
+import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.signature.StringSignature;
 
 import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
@@ -14,23 +16,38 @@ import jp.wasabeef.glide.transformations.CropCircleTransformation;
 public class ImageLoad {
     private static ImageLoad instance;
 
-    private ImageLoad () {
+    private ImageLoad() {
 
     }
 
-    public static ImageLoad getInstance () {
-        if(instance == null){
+    public static ImageLoad getInstance() {
+        if (instance == null) {
             instance = new ImageLoad();
         }
 
         return instance;
     }
 
-    public void load(Context context, ImageView imageView , int resource){
-        Glide.with(context).load(resource).into(imageView);
+    public void load(Context context, ImageView imageView, int resource) {
+        Glide.with(context)
+                .load(resource)
+                .into(imageView);
     }
 
-    public void circleLoad(Context context, ImageView imageView , int resource){
+    public void load(Context context, ImageView imageView, String resource) {
+        Glide.with(context)
+                .load(resource)
+                .into(imageView);
+    }
+
+    public void load(Context context, ImageView imageView, Uri resource) {
+        Glide.with(context)
+                .load(resource)
+                .signature(new StringSignature(String.valueOf(System.currentTimeMillis())))
+                .into(imageView);
+    }
+
+    public void circleLoad(Context context, ImageView imageView, int resource) {
         Glide.with(context).load(resource)
                 .bitmapTransform(new CropCircleTransformation(context)).into(imageView);
     }
